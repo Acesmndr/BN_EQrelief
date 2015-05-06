79d53839afa1b82a9024dc3060d0cf6c3ba810ca
acesmndr <acesmndr@gmail.com>
2015-05-06 23:38:34 +0545

    Finalized Result of InfoGraphics

--- js/functions.js
+++ js/functions.js
@@ -4,31 +4,64 @@ var tempp;
 var geocoder=new google.maps.Geocoder();
 var distList=new Array();
 var house={};
-function districtPercentage(){
+var house1={};
+var markersArray=new Array();
+var opaCT;
+google.maps.Map.prototype.clearOverlays = function() {
+  for (var i = 0; i < markersArray.length; i++ ) {
+    markersArray[i].setMap(null);
+  }
+  markersArray.length = 0;
+}
+function districtPercentage(sel_opt){
 		$.ajax({
 			 url: "assets/TotalAnalytics.json",
 			 dataType: 'json',
         		 type: 'GET',
 			 success: function (data) {
-                    
+                    var sel_list=["No. of Households served","Additional No. of Households","BN Non-Medical Volunteers","Medical Volunteers","Partner Volunteers","Tents","Noodles (in Cartons)","Biscuits (in Cartons)","Rice (in Sacks)","Beaten Rice (in Sacks)","Water (in Cartons)","Clothes (in Sacks)","Others"];
+                    sel_opt=sel_list[sel_opt];
+                    $("#info-box2").html(sel_opt);
+                    map.clearOverlays();
+                    house={};
 					a=data;
 					for(i=0;i<a["District"].length;i++){
-                            house[a["District"][i].toUpperCase()]=parseInt(a["No. of Households served"][i]);
+                            house["a"+distNo[i]]=parseInt(a[sel_opt][i]);
+                             house1["a"+distNo[i]]=parseInt(a[sel_opt][i]);
                             console.log("Hello");
                             }
 					map.data.setStyle(function(feature) {
                     sss=dist;
                             var temp=feature.getProperty("DISTRICT");
+                            console.log(house["a"+feature.getId()]);
                             tempp=feature;
 							//console.log(temp, dist[temp]);
-							if(house[temp]>0){
+							if(house["a"+feature.getId()]!=undefined){
+                                if(house["a"+feature.getId()]>100){     
+                                            opaCT=0.8;
+                                }else{
+                                    if(house["a"+feature.getId()]>50){
+                                            opaCT=0.6;
+                                    }else{
+                                        if(house["a"+feature.getId()]>30){
+                                            opaCT=0.4;
+                                            }else{
+                                                if(house["a"+feature.getId()]>0){
+                                                    opaCT=0.2;
+                                                    }else{
+                                                        opaCT=0;
+                                                    }
+                                            }
+                                    }
+                                }
                                 //distList.push(feature.getId());
-                                clr="Blue"
+                                clr= "rgba(0,128,255,"+opaCT+")"//"+(0.6+house["a"+feature.getId()]/1000)+")";
                             }else{
-                                clr="White";
+                                clr="rgba(0,0,0,0)";
                                 }
 						    return /** @type {google.maps.Data.StyleOptions} */({
 						      fillColor:clr,// feature.getProperty('color'),
+                              fillOpacity:1,  
 						      strokeWeight: 1.2,
 						      strokeOpacity: 1	
 						    });	
@@ -38,7 +71,6 @@ function districtPercentage(){
 
 		}
 		}).done(function(){
-        console.log("This is",distList);
         $.ajax({
         	url: "assets/DistrictHQ_Listbyaces.geojson",
     		dataType: 'json',
@@ -46,16 +78,19 @@ function districtPercentage(){
         	success: function (data) {
                 t2=data;
                 for(i=0;i<distNo.length;i++){
+                    //console.log(t1.features[distNo[i]].properties["DISTRICT"]+" "+house[t1.features[distNo[i]].properties["DISTRICT"]])
                      var marker = new MarkerWithLabel({
                                                         position: new google.maps.LatLng(t2.features[distNo[i]].geometry.coordinates[1],t2.features[distNo[i]].geometry.coordinates[0]),
                                                         map: map,
                                                         draggable: true,
                                                         raiseOnDrag: true,
-                                                        labelContent: t1.features[distNo[i]].properties["DISTRICT"],
-                                                        labelAnchor: new google.maps.Point(10, 35),
+                                                        /*labelContent: t1.features[distNo[i]].properties["DISTRICT"].slice(0,3)+" S:"+house["a"+distNo[i]]+" R:"+house1["a"+distNo[i]],*/
+                                                        labelContent:t1.features[distNo[i]].properties["DISTRICT"].slice(0,3)+" "+house["a"+distNo[i]],                    
+                                                        labelAnchor: new google.maps.Point(55, 35),
                                                         labelClass: "labels", // the CSS class for the label
                                                         labelInBackground: false
                                                      });
+                    markersArray.push(marker);
                             
                       
                 }
--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-var distNo=[49,52,51,41,54,21,29,36,31,37,53,45,27,48] // district served
+//var distNo=[49,52,51,41,54,21,29,36,31,37,53,45,27,48] // district served
+var distNo=[49,52,51,41,54,21,29,36,31,37,53,27,48] // district served
+
 var flag=0,a
 var map,t1,t2;
 function initialize() {
 	  map = new google.maps.Map(document.getElementById('map-canvas'), {
 	  	mapTypeId: google.maps.MapTypeId.ROADMAP,	
-	  	draggable: false,
-	  	zoom: 7,
+	  	draggable: true,
+	  	zoom: 9,
 	  	scrollwheel: false,
 	  	disableDoubleClickZoom: true,
 	  	disableDefaultUI: true,	
-	  	center: {lat: 28.05259082333986, lng: 84.1552734375}
+	  	center: {lat: 27.837669, lng: 85.326476}
 		});
 	$.ajax({
         	url: "assets/nepal-district.geojson.packed",
@@ -24,31 +26,10 @@ function initialize() {
 			t1.features[i].geometry.coordinates[0][0][1]=Math.floor(t1.features[i].geometry.coordinates[0][0][1] * 1000000) / 1000000;
 			}
         		map.data.addGeoJson(t1);
-			districtPercentage();
+			districtPercentage(0);
   		}
        		});	
      var a=new Array();
-     /*$.ajax({
-        	url: "assets/nepal-district-headquarters.geojson",
-		dataType: 'json',
-        	type: 'GET',
-        	success: function (data) {
-			    t2=data;
-                for(i=0;i<75;i++){
-                    var marker = new MarkerWithLabel({
-                        position: new google.maps.LatLng(t2.features[i].geometry.coordinates[1],t2.features[i].geometry.coordinates[0]),
-                        map: map,
-                        draggable: true,
-                        raiseOnDrag: true,
-                        labelContent: "100",
-                        labelAnchor: new google.maps.Point(10, 35),
-                        labelClass: "labels", // the CSS class for the label
-                        labelInBackground: false
-                     });
-                   }
-			}			
-            });	*/   
-	
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
